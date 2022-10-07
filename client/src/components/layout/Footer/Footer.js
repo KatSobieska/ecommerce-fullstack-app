be2a1e8e902006871ecc241b3c8ca8bb28f8dd60
KatSobieska <97301917+KatSobieska@users.noreply.github.com>
2022-10-07 16:29:16 +0200

    Add icons to footer and responsive styles

--- client/src/components/layout/Footer/Footer.js
+++ client/src/components/layout/Footer/Footer.js
@@ -1,10 +1,46 @@
 import { Container } from 'react-bootstrap';
 import styles from './Footer.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faFacebook,
+  faInstagram,
+  faTwitter,
+} from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => {
   return (
-    <Container className={`${styles.footer} d-flex justify-content-center`}>
-      Copyright © Ruby Star Jewelry 2022
+    <Container className={`${styles.footer}`}>
+      <div>
+        <ul className={`${styles.icons} d-flex justify-content-center`}>
+          <li>
+            <a href="#">
+              <FontAwesomeIcon
+                icon={faFacebook}
+                className={styles.icon}
+              ></FontAwesomeIcon>{' '}
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <FontAwesomeIcon
+                icon={faInstagram}
+                className={styles.icon}
+              ></FontAwesomeIcon>{' '}
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <FontAwesomeIcon
+                icon={faTwitter}
+                className={styles.icon}
+              ></FontAwesomeIcon>{' '}
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div className="d-flex justify-content-center">
+        Copyright © Mood Jewellery 2022
+      </div>
     </Container>
   );
 };
