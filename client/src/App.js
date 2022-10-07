f60480b8611533c2ca7186709668425e2feb9602
KatSobieska <97301917+KatSobieska@users.noreply.github.com>
2022-10-07 16:28:04 +0200

    Add routes to app

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,19 @@
 import { Container } from 'react-bootstrap';
+import { Route, Routes } from 'react-router-dom';
+import Cart from './components/features/Cart/Cart';
 import Footer from './components/layout/Footer/Footer';
 import Header from './components/layout/Header/Header';
+import Homepage from './components/pages/Homepage/Homepage';
 
 function App() {
   return (
     <main>
       <Container>
         <Header />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
         <Footer />
       </Container>
     </main>
