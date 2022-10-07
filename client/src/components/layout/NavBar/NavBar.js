c8b24306113a85669e1fc1be97d247ccf42cb0b0
KatSobieska <97301917+KatSobieska@users.noreply.github.com>
2022-10-07 16:30:12 +0200

    Add links to navbar and responsive styles

--- client/src/components/layout/NavBar/NavBar.js
+++ client/src/components/layout/NavBar/NavBar.js
@@ -1,5 +1,22 @@
+import { Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import styles from './NavBar.module.scss';
+
 const NavBar = () => {
-  return <div>NavBar</div>;
+  return (
+    <Navbar className={`${styles.navbar} justify-content-end`}>
+      <Nav>
+        <Nav.Link as={Link} to="/">
+          Home
+        </Nav.Link>
+      </Nav>
+      <Nav>
+        <Nav.Link as={Link} to="/cart">
+          Cart
+        </Nav.Link>
+      </Nav>
+    </Navbar>
+  );
 };
 
 export default NavBar;
