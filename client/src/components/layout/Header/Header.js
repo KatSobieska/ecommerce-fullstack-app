04e0d55edce7bbf29dd55cf4514654ed1216d3cd
KatSobieska <97301917+KatSobieska@users.noreply.github.com>
2022-10-07 16:30:46 +0200

    Add logo to header and responsive styles

--- client/src/components/layout/Header/Header.js
+++ client/src/components/layout/Header/Header.js
@@ -1,7 +1,17 @@
 import NavBar from '../NavBar/NavBar';
+import styles from './Header.module.scss';
 
 const Header = () => {
-  return <NavBar />;
+  return (
+    <div className={styles.logo}>
+      <img
+        className={`mx-auto d-block `}
+        src={`/images/logo_final.jpg`}
+        alt="logo"
+      ></img>
+      <NavBar />
+    </div>
+  );
 };
 
 export default Header;
