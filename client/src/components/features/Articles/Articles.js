919a9b96af8d5df2e7a19b89d0ef0742b0f4fcfc
KatSobieska <97301917+KatSobieska@users.noreply.github.com>
2022-10-09 15:21:21 +0200

    Optimize component to render based on state and change styles for button

--- client/src/components/features/Articles/Articles.js
+++ client/src/components/features/Articles/Articles.js
@@ -1,68 +1,34 @@
 import { Button, Card, Col, Row } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { getAllArticles } from '../../../redux/articlesRedux';
 import styles from './Articles.module.scss';
+import { useSelector } from 'react-redux';
 
 const Articles = () => {
+  const articles = useSelector(getAllArticles);
+
   return (
     <section className="mt-3 mb-3 d-flex justify-content-center">
       <Row className="mt-5 mb-5">
-        <Col>
-          <Card style={{ width: '22rem' }}>
-            <Card.Body>
-              <Card.Img
-                src={`/images/bluejew.jpg`}
-                className="mb-4"
-                style={{ height: '10rem', objectFit: 'cover' }}
-              />
-              <Card.Text className={styles.text}>
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
-                text ever since the 1500s.
-              </Card.Text>
-              <Link className={styles.link}>
-                <Button className={styles.button}>Read more..</Button>
-              </Link>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col>
-          <Card style={{ width: '22rem' }}>
-            <Card.Body>
-              <Card.Img
-                src={`/images/greenjew.jpg`}
-                className="mb-4"
-                style={{ height: '10rem', objectFit: 'cover' }}
-              />
-              <Card.Text className={styles.text}>
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
-                text ever since the 1500s.
-              </Card.Text>
-              <Link className={styles.link}>
-                <Button className={styles.button}>Read more..</Button>
-              </Link>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col>
-          <Card style={{ width: '22rem' }}>
-            <Card.Body>
-              <Card.Img
-                src={`/images/purplejew.jpg`}
-                className="mb-4"
-                style={{ height: '10rem', objectFit: 'cover' }}
-              />
-              <Card.Text className={styles.text}>
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
-                text ever since the 1500s.
-              </Card.Text>
-              <Link className={styles.link}>
-                <Button className={styles.button}>Read more..</Button>
-              </Link>
-            </Card.Body>
-          </Card>
-        </Col>
+        {articles.map((article, index) => (
+          <Col key={index}>
+            <Card style={{ width: '22rem' }}>
+              <Card.Body>
+                <Card.Img
+                  src={`/images/${article.image}`}
+                  className="mb-4"
+                  style={{ height: '10rem', objectFit: 'cover' }}
+                />
+                <Card.Text className={styles.text}>
+                  {article.descriptionShort}
+                </Card.Text>
+                <Link className={styles.link} to={'/' + article.id}>
+                  <Button className={styles.button}>Read more..</Button>
+                </Link>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
       </Row>
     </section>
   );
