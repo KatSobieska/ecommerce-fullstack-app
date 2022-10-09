de53fcb2598e829795f7a016afc832a94ac309cc
KatSobieska <97301917+KatSobieska@users.noreply.github.com>
2022-10-09 15:14:57 +0200

    Create initialState

---
+++ client/src/redux/initialState.js
@@ -0,0 +1,39 @@
+const initialState = {
+  articles: [
+    {
+      id: 'article-1',
+      descriptionShort:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+      descriptionLong:
+        "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.",
+      image: 'bluejew.jpg',
+      author: 'Amanda Doe',
+    },
+    {
+      id: 'article-2',
+      descriptionShort:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+      descriptionLong:
+        "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.",
+      image: 'greenjew.jpg',
+      author: 'John Doe',
+    },
+    {
+      id: 'article-3',
+      descriptionShort:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+      descriptionLong:
+        "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.",
+      image: 'purplejew.jpg',
+      author: ' Oscar Burton',
+    },
+  ],
+
+  ads: [
+    { id: 'ad1', image: 'carojew7.jpeg' },
+    { id: 'ad2', image: 'carojew6.jpeg' },
+    { id: 'ad3', image: 'carojew8.jpeg' },
+  ],
+};
+
+export default initialState;
