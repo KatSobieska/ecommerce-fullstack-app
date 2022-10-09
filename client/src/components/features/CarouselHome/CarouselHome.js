b99c650e1565dbdccfd65b86f479e1156a9903c6
KatSobieska <97301917+KatSobieska@users.noreply.github.com>
2022-10-09 15:17:26 +0200

    Add component with carousel with responsive styles

---
+++ client/src/components/features/CarouselHome/CarouselHome.js
@@ -0,0 +1,27 @@
+import { Carousel } from 'react-bootstrap/';
+import { useSelector } from 'react-redux';
+import { getAllAds } from '../../../redux/adsRedux';
+import styles from './CarouselHome.module.scss';
+
+const CarouselHome = () => {
+  const adsData = useSelector(getAllAds);
+  return (
+    <Carousel className={styles.carousel} indicators={false} variant="dark">
+      {adsData.map((ad, index) => (
+        <Carousel.Item key={index} className={styles.carousel}>
+          <img
+            className="d-block justify-content-center w-100"
+            src={`images/${ad.image}`}
+            alt="First slide"
+          />
+          <Carousel.Caption className={styles.caption}>
+            <h1>We do jewellery</h1>
+            <h3>to make women feel beautiful</h3>
+          </Carousel.Caption>
+        </Carousel.Item>
+      ))}
+    </Carousel>
+  );
+};
+
+export default CarouselHome;
