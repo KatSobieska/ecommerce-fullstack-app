9d336de2301c80ffb38f7433192b918f80f69a99
KatSobieska <97301917+KatSobieska@users.noreply.github.com>
2022-10-12 18:29:28 +0200

    Add collapse to navbar and update styles

--- client/src/components/layout/NavBar/NavBar.js
+++ client/src/components/layout/NavBar/NavBar.js
@@ -1,20 +1,27 @@
-import { Nav, Navbar } from 'react-bootstrap';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import styles from './NavBar.module.scss';
 
 const NavBar = () => {
   return (
-    <Navbar className={`${styles.navbar} justify-content-end`}>
-      <Nav>
-        <Nav.Link as={Link} to="/">
-          Home
-        </Nav.Link>
-      </Nav>
-      <Nav>
-        <Nav.Link as={Link} to="/cart">
-          Cart
-        </Nav.Link>
-      </Nav>
+    <Navbar
+      sticky="top"
+      expand="sm"
+      className={`${styles.navbar} justify-content-end`}
+    >
+      <Container>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+          <Nav className={`px-5 my-2 my-lg-0 ${styles.nav}`}>
+            <Nav.Link as={Link} to="/">
+              Home
+            </Nav.Link>
+            <Nav.Link as={Link} to="/cart">
+              Cart
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
     </Navbar>
   );
 };
