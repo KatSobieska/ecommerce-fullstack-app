106e2169b940884dd6d571a2b71f874644006d1c
KatSobieska <97301917+KatSobieska@users.noreply.github.com>
2022-10-12 18:28:49 +0200

    Add more content to header and update styles

--- client/src/components/layout/Header/Header.js
+++ client/src/components/layout/Header/Header.js
@@ -1,15 +1,37 @@
-import NavBar from '../NavBar/NavBar';
+import { Col, Nav, Navbar, Row } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import styles from './Header.module.scss';
 
 const Header = () => {
   return (
     <div className={styles.logo}>
-      <img
-        className={`mx-auto d-block `}
-        src={`/images/logo_final.jpg`}
-        alt="logo"
-      ></img>
-      <NavBar />
+      <Row>
+        <Col md={10}>
+          <img
+            className={`mx-auto  `}
+            src={`/images/logo_final.jpg`}
+            alt="logo"
+          ></img>
+        </Col>
+        <Col>
+          <Navbar className="d-flex justify-content-end">
+            <Nav className={styles.nav}>
+              <Nav.Link as={Link} to="/">
+                About
+              </Nav.Link>
+              <Nav.Link as={Link} to="/cart">
+                Contact
+              </Nav.Link>
+              <Nav.Link as={Link} to="/cart">
+                Login
+              </Nav.Link>
+              <Nav.Link as={Link} to="/cart">
+                Register
+              </Nav.Link>
+            </Nav>
+          </Navbar>
+        </Col>
+      </Row>
     </div>
   );
 };
