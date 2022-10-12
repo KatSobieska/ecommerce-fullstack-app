ae289118e12814d2e1ea58a67fec4b62b79ab63f
KatSobieska <97301917+KatSobieska@users.noreply.github.com>
2022-10-12 18:27:53 +0200

    Add more content to footer and update styles

--- client/src/components/layout/Footer/Footer.js
+++ client/src/components/layout/Footer/Footer.js
@@ -1,4 +1,4 @@
-import { Container } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
 import styles from './Footer.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
@@ -9,39 +9,69 @@ import {
 
 const Footer = () => {
   return (
-    <Container className={`${styles.footer}`}>
-      <div>
-        <ul className={`${styles.icons} d-flex justify-content-center`}>
-          <li>
-            <a href="#">
-              <FontAwesomeIcon
-                icon={faFacebook}
-                className={styles.icon}
-              ></FontAwesomeIcon>{' '}
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <FontAwesomeIcon
-                icon={faInstagram}
-                className={styles.icon}
-              ></FontAwesomeIcon>{' '}
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <FontAwesomeIcon
-                icon={faTwitter}
-                className={styles.icon}
-              ></FontAwesomeIcon>{' '}
-            </a>
-          </li>
-        </ul>
-      </div>
-      <div className="d-flex justify-content-center">
-        Copyright © Mood Jewellery 2022
-      </div>
-    </Container>
+    <div className={`${styles.footer}`}>
+      <Container>
+        <Row className="d-flex justify-content-center">
+          <Col xs={12} md={5} className="mt-5">
+            <div>
+              <ul className={`${styles.icons} d-flex`}>
+                <li>
+                  <a
+                    href="https://www.facebook.com"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <FontAwesomeIcon
+                      icon={faFacebook}
+                      className={styles.icon}
+                    ></FontAwesomeIcon>
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://www.instagram.com"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <FontAwesomeIcon
+                      icon={faInstagram}
+                      className={styles.icon}
+                    ></FontAwesomeIcon>
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://www.twitter.com"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <FontAwesomeIcon
+                      icon={faTwitter}
+                      className={styles.icon}
+                    ></FontAwesomeIcon>
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div>
+              <p>Copyright © Mood Jewellery 2022</p>
+            </div>
+          </Col>
+          <Col xs={12} md={3} className="mt-5">
+            <h5 className="pb-3">More information</h5>
+            <p>About shop</p>
+            <p>FAQ</p>
+            <p>Contact us</p>
+          </Col>
+          <Col xs={12} md={2} className="mt-5">
+            <h5 className="pb-3">Customer Service</h5>
+            <p>Payment methods</p>
+            <p>Returns</p>
+            <p>Terms and conditions</p>
+          </Col>
+        </Row>
+      </Container>
+    </div>
   );
 };
 
