920767154cf8be4d4de713b4f47165ee43733279
KatSobieska <97301917+KatSobieska@users.noreply.github.com>
2022-10-12 18:18:58 +0200

    Move header, navbar and footer out of container

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,34 @@
 import { Container } from 'react-bootstrap';
 import { Route, Routes } from 'react-router-dom';
+import AllProducts from './components/features/AllProducts/AllProducts';
 import Cart from './components/features/Cart/Cart';
+import Necklaces from './components/features/Necklaces/Necklaces';
+import OrderForm from './components/features/OrderForm/OrderForm';
 import Footer from './components/layout/Footer/Footer';
 import Header from './components/layout/Header/Header';
+import NavBar from './components/layout/NavBar/NavBar';
 import Article from './components/pages/Article/Article';
 import Homepage from './components/pages/Homepage/Homepage';
+import ProductPage from './components/pages/ProductPage/ProductPage';
 
 function App() {
   return (
     <main>
+      <Header />
+      <NavBar />
+
       <Container>
-        <Header />
         <Routes>
           <Route path="/" element={<Homepage />} />
           <Route path="/cart" element={<Cart />} />
-          <Route path="/:articleId" element={<Article />} />
+          <Route path="/articles/:articleId" element={<Article />} />
+          <Route path="/orderForm" element={<OrderForm />} />
+          <Route path="/allProducts" element={<AllProducts />} />
+          <Route path="/category/:category" element={<Necklaces />} />
+          <Route path="/:productId" element={<ProductPage />} />
         </Routes>
-        <Footer />
       </Container>
+      <Footer />
     </main>
   );
 }
