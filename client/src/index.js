22c4bc7c7fafcf785b51555c9e569aa1f0b9c178
KatSobieska <97301917+KatSobieska@users.noreply.github.com>
2022-10-12 18:19:15 +0200

    Import styles

--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
+import './styles/global.scss';
+import './styles/settings.scss';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
