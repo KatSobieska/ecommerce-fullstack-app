02894ffd7ea5f41e78962075320adc95b9ec62fd
KatSobieska <97301917+KatSobieska@users.noreply.github.com>
2022-10-13 19:57:09 +0200

    Move image to navbar, add links and change styles

--- client/src/components/layout/Header/Header.js
+++ client/src/components/layout/Header/Header.js
@@ -1,37 +1,28 @@
-import { Col, Nav, Navbar, Row } from 'react-bootstrap';
+import { Col, Container, Nav, Navbar, Row } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import styles from './Header.module.scss';
 
 const Header = () => {
   return (
-    <div className={styles.logo}>
-      <Row>
-        <Col md={10}>
-          <img
-            className={`mx-auto  `}
-            src={`/images/logo_final.jpg`}
-            alt="logo"
-          ></img>
-        </Col>
-        <Col>
-          <Navbar className="d-flex justify-content-end">
-            <Nav className={styles.nav}>
-              <Nav.Link as={Link} to="/">
-                About
-              </Nav.Link>
-              <Nav.Link as={Link} to="/cart">
-                Contact
-              </Nav.Link>
-              <Nav.Link as={Link} to="/cart">
-                Login
-              </Nav.Link>
-              <Nav.Link as={Link} to="/cart">
-                Register
-              </Nav.Link>
-            </Nav>
-          </Navbar>
-        </Col>
-      </Row>
+    <div className={styles.background}>
+      <Container className={styles.logo}>
+        <Row className="m-0">
+          <Col md={12}>
+            <Navbar className="d-flex justify-content-end">
+              <Nav className={styles.nav}>
+                <Nav.Link>About</Nav.Link>
+                <Nav.Link>Contact</Nav.Link>
+                <Nav.Link as={Link} to="/login">
+                  Login
+                </Nav.Link>
+                <Nav.Link as={Link} to="/register">
+                  Sign up
+                </Nav.Link>
+              </Nav>
+            </Navbar>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 };
--- client/src/components/layout/NavBar/NavBar.js
+++ client/src/components/layout/NavBar/NavBar.js
@@ -1,27 +1,50 @@
-import { Container, Nav, Navbar } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faCartShopping,
+  faHouse,
+  faSearch,
+} from '@fortawesome/free-solid-svg-icons';
+import { Col, Form, Nav, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import styles from './NavBar.module.scss';
 
 const NavBar = () => {
   return (
-    <Navbar
-      sticky="top"
-      expand="sm"
-      className={`${styles.navbar} justify-content-end`}
-    >
-      <Container>
+    <Navbar sticky="top" expand="sm" className={`${styles.navbar}`}>
+      <Col>
+        <img src={`/images/logo_final.jpg`} alt="logo"></img>
+      </Col>
+      <Col>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
-          <Nav className={`px-5 my-2 my-lg-0 ${styles.nav}`}>
+          <Nav className={`px-2 my-2 my-lg-0 `}>
+            <Form className="d-flex align-items-center">
+              <Form.Control
+                type="search"
+                className="me-2"
+                placeholder="Search"
+                aria-label="Search"
+              />
+            </Form>
             <Nav.Link as={Link} to="/">
-              Home
+              <FontAwesomeIcon icon={faSearch}> </FontAwesomeIcon>
+            </Nav.Link>
+            <Nav.Link as={Link} to="/">
+              <FontAwesomeIcon icon={faHouse} />
             </Nav.Link>
             <Nav.Link as={Link} to="/cart">
-              Cart
+              <FontAwesomeIcon icon={faCartShopping}> </FontAwesomeIcon>
+            </Nav.Link>
+            <Nav.Link
+              as={Link}
+              to="/cart"
+              className={`d-flex align-items-center justify-content-center m-2 pt-0  ${styles.cartQuantity}`}
+            >
+              {0}
             </Nav.Link>
           </Nav>
         </Navbar.Collapse>
-      </Container>
+      </Col>
     </Navbar>
   );
 };
