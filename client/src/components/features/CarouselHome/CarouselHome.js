4252c39ed711676ba2e76ece683693c607cea495
KatSobieska <97301917+KatSobieska@users.noreply.github.com>
2022-10-13 19:46:19 +0200

    Update carousel and its styles

--- client/src/components/features/CarouselHome/CarouselHome.js
+++ client/src/components/features/CarouselHome/CarouselHome.js
@@ -6,13 +6,18 @@ import styles from './CarouselHome.module.scss';
 const CarouselHome = () => {
   const adsData = useSelector(getAllAds);
   return (
-    <Carousel className={styles.carousel} indicators={false} variant="dark">
+    <Carousel
+      className={styles.carousel}
+      indicators={false}
+      controls={false}
+      variant="dark"
+    >
       {adsData.map((ad, index) => (
         <Carousel.Item key={index} className={styles.carousel}>
           <img
             className="d-block justify-content-center w-100"
             src={`images/${ad.image}`}
-            alt="First slide"
+            alt="Carousel slide"
           />
           <Carousel.Caption className={styles.caption}>
             <h1>We do jewellery</h1>
