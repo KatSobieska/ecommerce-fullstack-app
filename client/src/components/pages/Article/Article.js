6429c7d42719461bec326ad37ccd44505e3ae94f
KatSobieska <97301917+KatSobieska@users.noreply.github.com>
2022-10-30 20:11:07 +0100

    Correct formatting and view article by ID

--- client/src/components/pages/Article/Article.js
+++ client/src/components/pages/Article/Article.js
@@ -1,15 +1,14 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { Button, Card, Col, Row } from 'react-bootstrap';
-import { useSelector } from 'react-redux';
-import { Link, useParams } from 'react-router-dom';
-import { getArticleById } from '../../../redux/articlesRedux';
-import styles from './Article.module.scss';
-import { faArrowAltCircleLeft } from '@fortawesome/free-regular-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Button, Card, Col, Row } from "react-bootstrap";
+import { useSelector } from "react-redux";
+import { Link, useParams } from "react-router-dom";
+import { getArticleById } from "../../../redux/articlesRedux";
+import styles from "./Article.module.scss";
+import { faArrowAltCircleLeft } from "@fortawesome/free-regular-svg-icons";
 
 const Article = () => {
-  const { articleId } = useParams();
-  const articleData = useSelector((state) => getArticleById(state, articleId));
-  console.log('articleData', articleData);
+  const { id } = useParams();
+  const articleData = useSelector((state) => getArticleById(state, id));
 
   return (
     <section className={styles.article}>
@@ -17,9 +16,9 @@ const Article = () => {
         <Col xl={4}>
           <Card
             style={{
-              width: '22rem',
-              height: '18rem',
-              border: 'none',
+              width: "22rem",
+              height: "18rem",
+              border: "none",
             }}
           >
             <Card.Body className="">
@@ -27,16 +26,16 @@ const Article = () => {
                 src={`/images/${articleData.image}`}
                 className="mb-4"
                 style={{
-                  height: '15rem',
-                  objectFit: 'cover',
-                  borderRadius: '0',
+                  height: "15rem",
+                  objectFit: "cover",
+                  borderRadius: "0",
                 }}
               />
             </Card.Body>
           </Card>
         </Col>
         <Col xl={8}>
-          <Card style={{ height: '18rem', border: 'none' }}>
+          <Card style={{ height: "18rem", border: "none" }}>
             <Card.Body>
               <Card.Text className={styles.title}>
                 {articleData.title}
@@ -48,7 +47,7 @@ const Article = () => {
                 <b>Author:</b> {articleData.author}
               </Card.Text>
               <Row>
-                <Link className={styles.link} to={'/'}>
+                <Link className={styles.link} to={"/"}>
                   <Button className={styles.button}>
                     <FontAwesomeIcon
                       icon={faArrowAltCircleLeft}
