55c847cc88ff0f8e86058120602ef92755dc8dda
KatSobieska <97301917+KatSobieska@users.noreply.github.com>
2022-10-30 20:09:51 +0100

    Correct formatting and view all categories

--- client/src/components/features/Categories/Categories.js
+++ client/src/components/features/Categories/Categories.js
@@ -1,11 +1,20 @@
-import { Card, Col, Row } from 'react-bootstrap';
-import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
-import { getAllCategories } from '../../../redux/categoriesRedux';
-import styles from './Categories.module.scss';
+import { useEffect } from "react";
+import { Card, Col, Row } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import {
+  getAllCategories,
+  loadCategoriesRequest,
+} from "../../../redux/categoriesRedux";
+import styles from "./Categories.module.scss";
 
 const Categories = () => {
   const categories = useSelector(getAllCategories);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(loadCategoriesRequest());
+  }, [dispatch]);
 
   return (
     <section className={`${styles.header} "mt-5 mb-5 "`}>
@@ -15,21 +24,20 @@ const Categories = () => {
       <Row className="mt-3 mb-2">
         {categories.map((category, index) => (
           <Col key={index} className="p-0 d-flex justify-content-center">
-            <Card style={{ width: '19rem', border: 'none' }} className="mt-3 ">
-              <Card.Img
-                src={`/images/${category.image}`}
-                className="mb-4"
-                style={{
-                  height: '15rem',
-                  objectFit: 'cover',
-                  borderRadius: '0',
-                }}
-              />
+            <Card style={{ width: "19rem", border: "none" }} className="mt-3 ">
+              <Link to={"/" + category.id}>
+                <Card.Img
+                  src={`/images/${category.image}`}
+                  className="mb-4"
+                  style={{
+                    height: "15rem",
+                    objectFit: "cover",
+                    borderRadius: "0",
+                  }}
+                />
+              </Link>
               <Card.Body>
-                <Link
-                  className={styles.categoryLink}
-                  to={'/category/' + category.id}
-                >
+                <Link className={styles.categoryLink} to={"/" + category.id}>
                   <Card.Text>{category.name}</Card.Text>
                 </Link>
               </Card.Body>
@@ -38,8 +46,8 @@ const Categories = () => {
         ))}
       </Row>
       <Row>
-        <Link className={styles.link} to={'/allProducts'}>
-          <Card.Text>Check all products</Card.Text>
+        <Link className={styles.link} to={"/allProducts"}>
+          <Card.Text>View all</Card.Text>
         </Link>
       </Row>
     </section>
