4755ac2cb3396dcbc82a6b6ba0f5358ed3e905e1
KatSobieska <97301917+KatSobieska@users.noreply.github.com>
2022-10-30 20:08:57 +0100

    Correct formatting and view all articles

--- client/src/components/features/Articles/Articles.js
+++ client/src/components/features/Articles/Articles.js
@@ -1,11 +1,20 @@
-import { Button, Card, Col, Row } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-import { getAllArticles } from '../../../redux/articlesRedux';
-import styles from './Articles.module.scss';
-import { useSelector } from 'react-redux';
+import { Button, Card, Col, Row } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import {
+  getAllArticles,
+  loadArticlesRequest,
+} from "../../../redux/articlesRedux";
+import styles from "./Articles.module.scss";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
 
 const Articles = () => {
   const articles = useSelector(getAllArticles);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(loadArticlesRequest());
+  }, [dispatch]);
 
   return (
     <section className={`${styles.header} "mt-5 mb-5 "`}>
@@ -15,21 +24,21 @@ const Articles = () => {
       <Row className={`"mt-5 mb-5" ${styles.articles}`}>
         {articles.map((article, index) => (
           <Col key={index} className="p-0 d-flex justify-content-center">
-            <Card style={{ width: '22rem', border: 'none' }}>
+            <Card style={{ width: "22rem", border: "none" }}>
               <Card.Img
                 src={`/images/${article.image}`}
                 className="mb-4"
                 style={{
-                  height: '16rem',
-                  objectFit: 'cover',
-                  borderRadius: '0',
+                  height: "16rem",
+                  objectFit: "cover",
+                  borderRadius: "0",
                 }}
               />
               <Card.Body>
                 <Card.Text className={styles.text}>
                   {article.descriptionShort}
                 </Card.Text>
-                <Link className={styles.link} to={'/articles/' + article.id}>
+                <Link className={styles.link} to={"/articles/" + article._id}>
                   <Button className={styles.button}>Read more..</Button>
                 </Link>
               </Card.Body>
