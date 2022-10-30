21cdc7253afe64eb64a20e2d17749e467d625dc3
KatSobieska <97301917+KatSobieska@users.noreply.github.com>
2022-10-30 20:08:36 +0100

    Correct formatting and view all products

--- client/src/components/features/AllProducts/AllProducts.js
+++ client/src/components/features/AllProducts/AllProducts.js
@@ -1,17 +1,26 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowAltCircleLeft } from '@fortawesome/free-regular-svg-icons';
-import { Button, Card, Col, Row } from 'react-bootstrap';
-import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
-import { getAllProducts } from '../../../redux/productsRedux';
-import styles from './AllProducts.module.scss';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowAltCircleLeft } from "@fortawesome/free-regular-svg-icons";
+import { Button, Card, Col, Row } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import {
+  getAllProducts,
+  loadProductsRequest,
+} from "../../../redux/productsRedux";
+import styles from "./AllProducts.module.scss";
+import { useEffect } from "react";
 
 const AllProducts = () => {
   const products = useSelector(getAllProducts);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(loadProductsRequest());
+  }, [dispatch]);
   return (
     <section className="mt-5 mb-5">
       <Row>
-        <Link className={styles.link} to={'/'}>
+        <Link className={styles.link} to={"/"}>
           <Button className={` mb-5 ${styles.button}`}>
             <FontAwesomeIcon icon={faArrowAltCircleLeft}></FontAwesomeIcon>Back
           </Button>
@@ -21,12 +30,12 @@ const AllProducts = () => {
         {products.map((product, index) => (
           <Col key={index}>
             <Card
-              style={{ width: '18rem', borderRadius: '0' }}
+              style={{ width: "18rem", borderRadius: "0" }}
               className="mt-2 mb-2"
             >
               <Card.Img
                 src={`/images/${product.image}`}
-                style={{ height: '10rem', objectFit: 'cover' }}
+                style={{ height: "10rem", objectFit: "cover" }}
               />
               <Card.Text className={styles.category}>
                 {product.category}
@@ -36,7 +45,7 @@ const AllProducts = () => {
                   {product.name}
                 </Card.Text>
                 <Card.Text className={styles.price}>{product.price}</Card.Text>
-                <Link className={styles.link} to={'/' + product.id}>
+                <Link className={styles.link} to={"/products/" + product._id}>
                   <Button className={styles.button}>Check details</Button>
                 </Link>
               </Card.Body>
