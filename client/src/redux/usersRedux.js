3233602399d551cb697c5fbd15bba8d5b2303e72
KatSobieska <97301917+KatSobieska@users.noreply.github.com>
2022-10-30 20:06:14 +0100

    Add reducer for Users

---
+++ client/src/redux/usersRedux.js
@@ -0,0 +1,27 @@
+export const getUser = ({ user }) => user;
+
+const reducerName = 'users';
+const createActionName = (name) => `app/${reducerName}/${name}`;
+
+const LOG_IN = createActionName('LOG_IN');
+const LOG_OUT = createActionName('LOG_OUT');
+
+export const logIn = (payload) => ({ type: LOG_IN, payload });
+export const logOut = (payload) => ({ type: LOG_OUT, payload });
+
+const initialState = {
+  user: null,
+};
+
+const usersReducer = (statePart = initialState, action) => {
+  switch (action.type) {
+    case LOG_IN:
+      return action.payload;
+    case LOG_OUT:
+      return null;
+    default:
+      return statePart;
+  }
+};
+
+export default usersReducer;
