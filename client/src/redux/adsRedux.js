bb8642267ec4a59e59500e4b6ce61981163afeae
KatSobieska <97301917+KatSobieska@users.noreply.github.com>
2022-10-30 20:04:53 +0100

    Update reducer for Ads

--- client/src/redux/adsRedux.js
+++ client/src/redux/adsRedux.js
@@ -1,7 +1,74 @@
-export const getAllAds = (state) => state.ads;
+import axios from "axios";
+import { API_URL } from "../config";
 
-export default function reducer(statePart = [], action = []) {
+export const getAllAds = ({ ads }) => ads.data;
+
+const reducerName = "ads";
+const createActionName = (name) => `app/${reducerName}/${name}`;
+
+const LOAD_ADS = createActionName("LOAD_ADS");
+
+const START_REQUEST = createActionName("START_REQUEST");
+const END_REQUEST = createActionName("END_REQUEST");
+const ERROR_REQUEST = createActionName("ERROR_REQUEST");
+
+export const loadAds = (payload) => ({ payload, type: LOAD_ADS });
+export const startRequest = (payload) => ({ payload, type: START_REQUEST });
+export const endRequest = (payload) => ({ payload, type: END_REQUEST });
+export const errorRequest = (payload) => ({ payload, type: ERROR_REQUEST });
+
+export const loadAdsRequest = () => {
+  return async (dispatch) => {
+    dispatch(startRequest({ name: "LOAD_ADS" }));
+    try {
+      const res = await axios.get(`${API_URL}/api/ads`);
+      dispatch(loadAds(res.data));
+      dispatch(endRequest({ name: "LOAD_ADS" }));
+    } catch (err) {
+      dispatch(errorRequest({ name: "LOAD_ADS", error: err.message }));
+    }
+  };
+};
+
+const initialState = {
+  data: [],
+  request: {
+    pending: false,
+    error: null,
+    success: null,
+  },
+};
+export default function reducer(statePart = initialState, action = []) {
   switch (action.type) {
+    case LOAD_ADS:
+      return { ...statePart, data: [...action.payload] };
+    case START_REQUEST:
+      return {
+        ...statePart,
+        request: {
+          pending: true,
+          error: null,
+          success: false,
+        },
+      };
+    case END_REQUEST:
+      return {
+        ...statePart,
+        request: {
+          pending: false,
+          error: null,
+          success: true,
+        },
+      };
+    case ERROR_REQUEST:
+      return {
+        ...statePart,
+        request: {
+          pending: false,
+          error: action.payload.error,
+          success: false,
+        },
+      };
     default:
       return statePart;
   }
