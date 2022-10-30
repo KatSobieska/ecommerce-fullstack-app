4c4fbc9ce806204d62957c1a618a18d83c587ad2
KatSobieska <97301917+KatSobieska@users.noreply.github.com>
2022-10-30 20:04:24 +0100

    Update store

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,23 +1,29 @@
-import articlesReducer from './articlesRedux';
-import adsReducer from './adsRedux';
-import categoriesReducer from './categoriesRedux';
-import productsReducer from './productsRedux';
-import { combineReducers, createStore } from 'redux';
-import initialState from './initialState';
+import articlesReducer from "./articlesRedux";
+import adsReducer from "./adsRedux";
+import categoriesReducer from "./categoriesRedux";
+import productsReducer from "./productsRedux";
+import cartReducer from "./cartRedux";
+import { applyMiddleware, combineReducers, compose, createStore } from "redux";
+import usersReducer from "./usersRedux";
+import thunk from "redux-thunk";
 
-const reducers = {
+const subreducers = {
   articles: articlesReducer,
   ads: adsReducer,
   categories: categoriesReducer,
   products: productsReducer,
+  cart: cartReducer,
+  user: usersReducer,
 };
 
-const combinedReducers = combineReducers(reducers);
+const rootReducer = combineReducers(subreducers);
 
 const store = createStore(
-  combinedReducers,
-  initialState,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  rootReducer,
+  compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
 );
 
 export default store;
