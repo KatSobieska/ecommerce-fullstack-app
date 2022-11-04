5c5090effaa1eaa61fb982caa3c3a4a560db3e5a
KatSobieska <97301917+KatSobieska@users.noreply.github.com>
2022-11-04 14:15:20 +0100

    Remove comment

--- client/src/components/pages/Register/Register.js
+++ client/src/components/pages/Register/Register.js
@@ -1,43 +1,43 @@
-import { useState } from 'react';
-import { Alert, Button, Form, Spinner } from 'react-bootstrap';
-import styles from './Register.module.scss';
+import { useState } from "react";
+import { Alert, Button, Form, Spinner } from "react-bootstrap";
+import { API_URL } from "../../../config";
+import styles from "./Register.module.scss";
 
 const Register = () => {
-  const [login, setLogin] = useState('');
-  const [password, setPassword] = useState('');
-  const [email, setEmail] = useState('');
+  const [login, setLogin] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
   const [status, setStatus] = useState(null);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // console.log(login, password, phone, avatar);
-    // const fd = new FormData();
-    // fd.append('login', login);
-    // fd.append('password', password);
-    // fd.append('phone', phone);
-    // fd.append('avatar', avatar);
+    console.log(login, password, email);
+    const fd = new FormData();
+    fd.append("login", login);
+    fd.append("password", password);
+    fd.append("email", email);
 
-    // const options = {
-    //   method: 'POST',
-    //   body: fd,
-    // };
+    const options = {
+      method: "POST",
+      body: fd,
+    };
 
-    // setStatus('loading');
-    // fetch(`${API_URL}/auth/register`, options)
-    //   .then((res) => {
-    //     if (res.status === 201) {
-    //       setStatus('success');
-    //     } else if (res.status === 400) {
-    //       setStatus('clientError');
-    //     } else if (res.status === 409) {
-    //       setStatus('loginError');
-    //     } else {
-    //       setStatus('serverError');
-    //     }
-    //   })
-    //   .catch((err) => {
-    //     setStatus('serverError');
-    //   });
+    setStatus("loading");
+    fetch(`${API_URL}/auth/register`, options)
+      .then((res) => {
+        if (res.status === 201) {
+          setStatus("success");
+        } else if (res.status === 400) {
+          setStatus("clientError");
+        } else if (res.status === 409) {
+          setStatus("loginError");
+        } else {
+          setStatus("serverError");
+        }
+      })
+      .catch((err) => {
+        setStatus("serverError");
+      });
   };
 
   return (
@@ -47,35 +47,35 @@ const Register = () => {
     >
       <h1 className={`my-4 ${styles.headerRegister}`}>Sign up</h1>
 
-      {status === 'success' && (
+      {status === "success" && (
         <Alert variant="success">
           <Alert.Heading>Success!</Alert.Heading>
           <p>You have been succesfully registered! You can now log in...</p>
         </Alert>
       )}
 
-      {status === 'serverError' && (
+      {status === "serverError" && (
         <Alert variant="danger">
           <Alert.Heading>Something went wrong...</Alert.Heading>
           <p>Unexpected error... Try again!</p>
         </Alert>
       )}
 
-      {status === 'clientError' && (
+      {status === "clientError" && (
         <Alert variant="danger">
           <Alert.Heading>No enough data</Alert.Heading>
           <p>You have to fill all the fields.</p>
         </Alert>
       )}
 
-      {status === 'loginError' && (
+      {status === "loginError" && (
         <Alert variant="warning">
           <Alert.Heading>Login already in use</Alert.Heading>
           <p>You have to use other login.</p>
         </Alert>
       )}
 
-      {status === 'loading' && (
+      {status === "loading" && (
         <Spinner animation="border" role="status" className="d-block mx-auto">
           <span className="visually-hidden">Loading...</span>
         </Spinner>
