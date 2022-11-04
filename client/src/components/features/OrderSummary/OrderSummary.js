35b974cb0dca54cc85d1981e61404189d4e4d180
KatSobieska <97301917+KatSobieska@users.noreply.github.com>
2022-11-04 14:11:41 +0100

    Update orderSummary about the state and update the styles

--- client/src/components/features/OrderSummary/OrderSummary.js
+++ client/src/components/features/OrderSummary/OrderSummary.js
@@ -1,24 +1,45 @@
-import { Button, Col, Row } from 'react-bootstrap';
-import styles from './OrderSummary.module.scss';
+import { Col, Row } from "react-bootstrap";
+import styles from "./OrderSummary.module.scss";
+import { useSelector } from "react-redux";
+import { getCartProducts } from "../../../redux/cartRedux";
+import { useEffect, useState } from "react";
 
 const OrderSummary = () => {
+  const cartData = useSelector(getCartProducts);
+  let [cartTotalPrice, setCartTotalPrice] = useState(0);
+  let [delivery] = useState(10);
+  let [totalOrderAmount] = useState(0);
+
+  useEffect(() => {
+    const totalCartCount = () => {
+      let cartTotalPrice = 0;
+      cartData.map((item) => (cartTotalPrice = item.price + cartTotalPrice));
+      setCartTotalPrice(cartTotalPrice);
+    };
+    totalCartCount();
+  }, [cartData]);
+
+  totalOrderAmount = delivery + cartTotalPrice;
+
   return (
     <section className={styles.summary}>
       <Row>
         <Col className="d-flex justify-content-center">Summary</Col>
       </Row>
       <Row className={`mt-5 d-flex flex-column ${styles.list}`}>
-        <Col className="m-3">Items: BLABLABLA</Col>
-        <Col className={`m-3 ${styles.line}`}>Order amount: 10$</Col>
+        <Col className="m-3">Items: </Col>
+        {cartData.map((cartItem) => (
+          <Col className="m-3">
+            {cartItem.name} {cartItem.price} $
+          </Col>
+        ))}
+        <Col className={`m-3 ${styles.line}`}>
+          Order amount: {cartTotalPrice}$
+        </Col>
 
-        <Col className={`m-3 ${styles.line}`}>Delivery: 10$</Col>
-        <Col className="m-3">Total: 20$</Col>
+        <Col className={`m-3 ${styles.line}`}>Delivery: {delivery}$</Col>
+        <Col className="m-3">Total: {totalOrderAmount}$</Col>
       </Row>
-      <Col className="d-flex justify-content-center">
-        <Button type="submit" className={styles.button}>
-          Process order
-        </Button>
-      </Col>
     </section>
   );
 };
